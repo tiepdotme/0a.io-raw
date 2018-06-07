674eeea0fe1a5df7ca71e3290279ddd4d70bbb0c
archywilhes <archy@zer0.degree>
2018-06-08 07:04:44 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ _.each(document.getElementsByClassName("ArchyItem"),function(item){
     var aLink = item.querySelector(".ArchyItemTitle a")
     var date = item.querySelector(".ArchyItemDate")
     var img =  item.querySelector(".ArchyItemImg")
+    if(date == null){
+        date = item.querySelector(".ArchyItemSubtitle")
+    }
     _.each([aLink,img],function(x){
     x.addEventListener("mouseover",function(){
         aLink.classList.add("hover")
