1d83449e607a1ce0c8c56971ae260dbe86d98e26
Archy Wilhes <a@castella.art>
2018-12-11 04:32:42 +0700

    update

--- js/main.js
+++ js/main.js
@@ -16,9 +16,10 @@ _.each(document.getElementsByClassName("ArchyItem"),function(item){
     })})
 })
 
-var l = document.getElementsByClassName("zeroDegreeLogo")[0]
+_.each(document.getElementsByClassName("zeroDegreeLogo"), function(l){
 l.addEventListener("click",function(){
     window.location.href = "http://0a.io"
+})
 })
 
 var zer0DLogo = document.getElementsByClassName("zeroDegreeLogo")[0]
